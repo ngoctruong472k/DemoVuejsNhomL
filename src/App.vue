d69face7bf7b3a45a7f1252c2ef4a7aead12c87e
Nguyen Ngoc Truong <ngoctruong472k@gmail.com>
2020-10-29 21:37:59 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div id="nav" class="logout">
+      <router-link
+        class="out"
+        v-if="authenticated"
+        to="/login"
+        v-on:click.native="logout()"
+        replace
+        >Log out</router-link
+      >
+    </div>
+    <router-view @authenticated="setAuthenticated" />
+  </div>
+</template>
+
+<script>
+import "./scss/style.scss";
+export default {
+  name: "App",
+  data() {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: "truong",
+        password: "123",
+      },
+      mockAccountUserUser: {
+        username: "ao",
+        password: "123",
+      },
+    };
+  },
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
+    },
+    logout() {
+      this.authenticated = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+$background: #f0f0f0;
+body {
+  background-color: $background;
+}
+
+
+</style>
---
+++ src/views/EditEmployee.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="bg-add">
+    <div class="add">
+      <div class="frf">
+        <div class="cancle">
+          <span></span>
+          <span></span>
+        </div>
+        <form>
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              id="nameInput"
+              v-model="employee.name"
+              placeholder="Name:"
+            />
+          </div>
+          <div class="form-group">
+            <!-- <label for="gmailInput">Email</label> -->
+            <input
+              type="text"
+              class="form-control"
+              id="gmailInput"
+              v-model="employee.gmail"
+              placeholder="Email:"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              pattern=""
+              id="phoneInput"
+              v-model="employee.phone"
+              placeholder="Phone:"
+            />
+          </div>
+          <div class="btn btn-primary" @click="clickSave()">save</div>
+          <div class="btn btn-primary" id="close" @click="clickThoat()">
+            Close
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+
+</style>
+<script>
+import "../scss/style.scss";
+export default {
+  name: "EditEmployee",
+  props: {
+    employee: Object,
+  },
+  methods: {
+    clickSave() {
+      this.$emit("save", this.employee);
+    },
+    clickThoat() {
+      this.$emit("nosave", this.employee);
+    },
+  },
+};
+</script>
